696e30d0d0009857f224c605f20e5c7c67c2e729
andrerim <andreasrim@gmail.com>
2021-09-08 17:03:38 +0200

    Add first draft of application

--- src/App.svelte
+++ src/App.svelte
@@ -1,33 +1,79 @@
 <script lang="ts">
-	interface Users {
-		[key: string]: {"username": string, "name": string}
-	}
-	export let name: string;
-	let users: Users = {
-        "user1": {
-            "username": "petter123",
-            "name": "petter"
-        },
-        "user2":{
-            "username": "lisa123",
-            "name": "lisa"
-	}
+  import { weekNumber } from "weeknumber";
+  const currentWeekNr = weekNumber(new Date());
+
+  interface User {
+    username: string;
+    name: string;
+    dutiesWeek: "even" | "odd";
+  }
+
+  interface Users extends Array<User> {}
+
+  export let name: string;
+
+  let users: Users = [
+    {
+      username: "petter123",
+      name: "petter",
+      dutiesWeek: "odd",
+    },
+    {
+      username: "lisa123",
+      name: "lisa",
+      dutiesWeek: "even",
+    },
+    {
+      username: "petter123",
+      name: "anders",
+      dutiesWeek: "odd",
+    },
+    {
+      username: "lisa123",
+      name: "rotmo",
+      dutiesWeek: "even",
+    },
+  ];
 </script>
 
 <main>
-  <h1>Hello {name}!</h1>
+  <header>
+    <h1>Vaskeliste @ Elvevegen</h1>
+    <h2>Week number: {currentWeekNr}</h2>
+  </header>
+
   <p>
-    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
-    how to build Svelte apps.
+    This means that
+    {#each users as user}
+      {#if user.dutiesWeek === "odd"}
+        {`${user.name} `}
+      {/if}
+    {/each}
+    is cleaning
   </p>
+
+  <h3>Duties:</h3>
+  <form>
+    <label> <input type="checkbox" />Vaske badet</label>
+    <label> <input type="checkbox" />Vaske kjøkkenet</label>
+    <label> <input type="checkbox" />Vaske stua</label>
+    <label> <input type="checkbox" />Vaske gangen</label>
+  </form>
+  <!-- TODO Add sub tasks and defention of done -->
+  <!-- TODO Firestore login -->
+  <!-- TODO add notification for all tasks completed -->
+  <!-- TODO Cleaning(leader)board - score points for tasks completed -->
 </main>
 
 <style>
   main {
-    text-align: center;
     padding: 1em;
     max-width: 240px;
     margin: 0 auto;
+  }
+
+  header {
+    text-align: center;
   }
 
   h1 {
